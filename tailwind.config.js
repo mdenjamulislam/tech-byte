d44db875b41b21182e72569f0530824c7288bfa0
mdenjamulislam <mdenjamulislam527@gmail.com>
2024-10-26 19:25:30 +0600

    Typography added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,51 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1rem',
+          md: '1rem',
+          lg: '1.5rem',
+          xl: '4rem',
+          '2xl': '10rem'
+        },
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1536px',
+          '3xl': '1600px'
+        }
+      }
+    },
+    colors: {
+      primary: "#172B4D",
+      accent: "#E2B203",
+      'darkPeach': '#BA9B88',
+      'textColor': '#2F2F2F',
+    },
+
+    fontFamily: {
+      'poppins': ["Poppins", "serif"],
+      'display': ["Oswald", "serif"],
+    },
+
+    fontSize: {
+      xs: '0.7rem',
+      sm: '0.8rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.5rem',
+      '2xl': '1.938rem',
+      '3xl': '2.438rem',
+      '4xl': '3rem',
+      '5xl': '3.875rem',
+      '6xl': '4rem',
+    },
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [],
 }
